3ed09e3a19a186620bde1d2b1d8ea398dd3589ee
= <=>
2016-12-03 14:31:06 +0000

    updates

--- luso.lua
+++ luso.lua
@@ -1,4 +1,4 @@
-#!/usr/bin/env lua
+#!/usr/bin/env lua5.3
 --socket = require "socket"
 -- socket.unix = require "socket.unix"
 
@@ -8,7 +8,7 @@ require "pl"
 --http = require("socket.http")
 unixHTTP= require("usock")
 assert(unixHTTP)
-json = require("json")
+json = require("cjson")
 assert(json)
 applyTemplate = require "pl.template".substitute
 
@@ -16,10 +16,11 @@ function updateConfig()
 	-- Get running containers
 	--b, c, h = http.request("http://localhost:8080/containers/json")
 	h, b = unixHttp.request("/containers/json")
-	--print("b",b)
+	-- print("b",b)
 	res = json.decode(b)
-	--print(pretty.write(res))
-	--print("----------")
+	-- for key, val in pairs(res) do print (key, val) end
+	print(pretty.write(res))
+	print("----------")
 	containers={}
 
 	for a, b in ipairs(res) do
@@ -27,26 +28,69 @@ function updateConfig()
 		rec.Id=b.Id
 		--strip front slash
 		rec.Name=string.sub(b.Names[1],2,-1)
+		print(rec.Name)
+		--print("n ports:"..#b.Ports) --should show length
+
 		--table.foreach(b.Ports, function(k,v)print( v.PrivatePort)end);
 		--if hasPort80(b.Ports) then print(rec.Name.." yup") else print(rec.Name.." nup") end
 		--if hasPort80(b.Ports) then
+		if b.Labels.hautoproxy then
+			print("Labels :")
+			print("hautoproxy:")
+			print(b.Labels.hautoproxy)
+			print("cname:")
+			print(b.Labels.cname)
+			print("domain:")
+			print(b.Labels.domain)
+			
+			if b.Labels.domain then
+				rec.Domain = b.Labels.domain
+			else
+				-- rec.Domain = "pump.ninja"
+				rec.Domain = os.getenv("HA_DOMAIN")
+			end
+
+			print("n ports:"..#b.Ports) --should show length
+			for key, value in ipairs(b.Ports) do 
+				print("Port:"..value.PrivatePort)
+				if value.PrivatePort == 80 then
+					print("got port 80")
+					-- we use SSL termination in haproxy
+					rec.Port = 80
+				elseif value.PrivatePort == 433 then
+					print("got port 433")
+					-- we use SSL termination in haproxy so this is kind double...
+					-- maybe use haproxy ssl through mode
+					rec.Port = 443
+				end
+			end
+			
+			rec.IP=getIP(rec.Name)
+			print("IP: "..rec.IP)
+			if (rec.Port) then
+				print("inserting container")
+				table.insert(containers,rec)
+			end
+
+		end
+		
 		if hasEnvHautoproxy(rec.Name) then
 			--rec.IP=b.Ports[1].IP
-			print(rec.Name.." has port 80")
 			--if hasEnvHautoproxy(rec.Name) then print("got hauto") else print("NO hauto") end
 			rec.IP=getIP(rec.Name)
+			print("got IP:"..rec.IP)
 			-- needs more love
-			rec.PrivatePort=80 --b.Ports[1].PrivatePort
-			rec.PublicPort=b.Ports[1].PublicPort
+			--rec.PrivatePort=80 --b.Ports[1].PrivatePort
 		end
 		--print("rec.Name:",rec.Name)
 		--print("rec.PrivatePort:",rec.PrivatePort)
 		--print("--")
-		if rec.PrivatePort == 80 then
-			table.foreach(rec, print)
-			print("")
-			table.insert(containers,rec)
-		end
+		--if rec.Port then
+			--table.foreach(rec, print)
+		--	for k, v in ipairs(rec) do print(v) end
+		--	print("inserting container")
+		--	table.insert(containers,rec)
+		--end
 	end
 
 	haptl=file.read("./haproxy.tmpl")
@@ -77,7 +121,7 @@ function getIP(name)
 	h, b = unixHttp.request("/containers/"..name.."/json")
 	--print("b",b)
 	res = json.decode(b)
-	print(pretty.write(res.NetworkSettings.IPAddress))
+	--print(pretty.write(res.NetworkSettings.IPAddress))
 	return res.NetworkSettings.IPAddress
 end
 
@@ -112,6 +156,24 @@ function hasEnvHautoproxy(name)
 
 end
 
+function hasLabelHautoproxy(name)
+	local gotHauto = false
+
+	h, b = unixHttp.request("/containers/"..name.."/json")
+	--print("b",b)
+	res = json.decode(b)
+	if res.Config.Env then
+		for k,item in ipairs(res.Config.Env) do
+			if string.find(item,"HAUTOPROXY") and string.find(item,"true") then
+				gotHauto = true
+			--	print("Env: "..item);
+			end	
+		end
+	end
+	return gotHauto
+
+end
+
 function HAProxyNeedsUpdate()
 	return os.execute("diff ./haproxy.cfg ./haproxy.cfg.old > /dev/null") ~= 0
 end
@@ -125,7 +187,7 @@ repeat
 
 	if HAProxyNeedsUpdate() then
 		print("Updating HAProxy")
-		restartHAProxy()
+		-- ENABLE TODO restartHAProxy()
 	end
 	if not sleep(15) then
 		--sigint ?
